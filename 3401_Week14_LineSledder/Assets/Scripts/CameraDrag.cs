3e298f697690ef8f26e4803af27f27ac3221a0e4
Josh Hoffman <jh276903@ohio.edu>
2019-04-22 23:44:39 -0400

    Added whole project

---
+++ 3401_Week14_LineSledder/Assets/Scripts/CameraDrag.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDrag : MonoBehaviour
+{
+    private float cameraDistance;
+    private Vector3 MouseStart;
+    
+    void Start()
+    {
+        cameraDistance = transform.position.z; 
+    }
+
+    void Update()
+    {
+        //Find Position of where mouse currently is
+        if (Input.GetMouseButtonDown(1))
+        {
+            MouseStart = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraDistance);
+            MouseStart = Camera.main.ScreenToWorldPoint(MouseStart);
+            MouseStart.z = transform.position.z;
+        }
+        //Move Camera Position relative to mouse position
+        else if (Input.GetMouseButton(1))
+        {
+            Vector3 MouseMove = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraDistance);
+            MouseMove = Camera.main.ScreenToWorldPoint(MouseMove);
+            MouseMove.z = transform.position.z;
+            transform.position = transform.position - (MouseMove - MouseStart);
+        }
+    }
+}
